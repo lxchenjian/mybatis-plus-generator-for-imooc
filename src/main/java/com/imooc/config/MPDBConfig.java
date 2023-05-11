8888d1ee8c34c7eefe1cfc3e525e49b6f4091fa3
lxchenjian <982861129@qq.com>
2023-05-12 00:26:53 +0800

    部分修改

--- src/main/java/com/imooc/config/MPDBConfig.java
+++ src/main/java/com/imooc/config/MPDBConfig.java
@@ -32,7 +32,7 @@ public class MPDBConfig {
         // 数据源配置
         DataSourceConfig dsc = new DataSourceConfig();
         dsc.setDbType(DbType.MYSQL);
-        dsc.setUrl("jdbc:mysql://192.168.1.111:3306/imooc-hire?useUnicode=true&useSSL=false&characterEncoding=utf8");
+        dsc.setUrl("jdbc:mysql://192.168.1.111:3306/liepin?useUnicode=true&useSSL=false&characterEncoding=utf8");
         // dsc.setSchemaName("public");
         dsc.setDriverName("com.mysql.cj.jdbc.Driver");
         dsc.setUsername("root");
